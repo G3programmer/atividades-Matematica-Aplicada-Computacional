4c211fae58aa2be1954f581010ec3684bff81087
G_03 <130713943+G3programmer@users.noreply.github.com>
2025-04-12 15:21:01 -0300

    Create activi 1.cpp

---
+++ March/activi 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+    float base, altura, retangulo;
+    cout << "Digite a base: ";
+    cin >> base;
+
+    cout << "Digite a altura: ";
+    cin >> altura;
+
+    retangulo = base * altura;
+
+    cout << "A area do retangulo é: " << retangulo << endl;
+    return 0;
+}
