b5064696e4a2ca2494394af67fc3ac53a9bd089e
G_03 <130713943+G3programmer@users.noreply.github.com>
2025-04-26 19:42:12 -0300

    Update Menu.cpp

--- April/Menu.cpp
+++ April/Menu.cpp
@@ -1,26 +1,34 @@
-/*1 - Hamburguer ................ R$ 15,00
-
+/* 
+1 - Hamburguer ................ R$ 15,00
 2 - Pizza ..................... R$ 30,00
-
 3 - Salada .................... R$ 12,00
-
 4 - Suco Natural .............. R$ 8,00
-
 5 - Refrigerante .............. R$ 6,00
-
---------------------------------*/
+--------------------------------
+*/
 #include <iostream>
 using namespace std;
-int main()
-{
-    int num1,num2;
-    cout<<" #=================#\n"
-            " |1- Hamburguer ................ R$ 15,00| \n"
-            " |2- Pizza ................ R$ 15,00| \n"
-            " |3- Salada ................ R$ 15,00| \n"
-            " |4- Suco Natural ................ R$ 15,00| \n"
-            " |5 Refrigerante ................ R$ 15,00| \n";
 
-    
+int main() {
+    int opcao;
+    cout << "#================ Menu ================#\n"
+            "| 1 - Hamburguer ............... R$ 15,00 |\n"
+            "| 2 - Pizza .................... R$ 30,00 |\n"
+            "| 3 - Salada ................... R$ 12,00 |\n"
+            "| 4 - Suco Natural ............. R$ 8,00  |\n"
+            "| 5 - Refrigerante ............. R$ 6,00  |\n"
+            "#=======================================#\n"
+            "Digite o número do seu pedido: ";
+    cin >> opcao;
+
+    switch (opcao) {
+        case 1: cout << "Você pediu: Hamburguer - R$ 15,00\n"; break;
+        case 2: cout << "Você pediu: Pizza - R$ 30,00\n"; break;
+        case 3: cout << "Você pediu: Salada - R$ 12,00\n"; break;
+        case 4: cout << "Você pediu: Suco Natural - R$ 8,00\n"; break;
+        case 5: cout << "Você pediu: Refrigerante - R$ 6,00\n"; break;
+        default: cout << "Opção inválida. Tente novamente.\n";
+    }
+
     return 0;
 }
