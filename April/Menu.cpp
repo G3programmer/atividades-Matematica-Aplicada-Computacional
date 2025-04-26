cd8ef59d2233d27cd67d649d49a5204844a83c1e
G_03 <130713943+G3programmer@users.noreply.github.com>
2025-04-26 19:59:09 -0300

    Update Menu.cpp

--- April/Menu.cpp
+++ April/Menu.cpp
@@ -11,24 +11,36 @@ using namespace std;
 
 int main() {
     int opcao;
-    cout << "#================ Menu ================#\n"
-            "| 1 - Hamburguer ............... R$ 15,00 |\n"
-            "| 2 - Pizza .................... R$ 30,00 |\n"
-            "| 3 - Salada ................... R$ 12,00 |\n"
-            "| 4 - Suco Natural ............. R$ 8,00  |\n"
-            "| 5 - Refrigerante ............. R$ 6,00  |\n"
-            "#=======================================#\n"
-            "Digite o número do seu pedido: ";
-    cin >> opcao;
+    char continuar;
+    float total = 0;
 
-    switch (opcao) {
-        case 1: cout << "Você pediu: Hamburguer - R$ 15,00\n"; break;
-        case 2: cout << "Você pediu: Pizza - R$ 30,00\n"; break;
-        case 3: cout << "Você pediu: Salada - R$ 12,00\n"; break;
-        case 4: cout << "Você pediu: Suco Natural - R$ 8,00\n"; break;
-        case 5: cout << "Você pediu: Refrigerante - R$ 6,00\n"; break;
-        default: cout << "Opção inválida. Tente novamente.\n";
-    }
+    do {
+        cout << "#================ Menu ================#\n"
+                "| 1 - Hamburguer ............... R$ 15,00 |\n"
+                "| 2 - Pizza .................... R$ 30,00 |\n"
+                "| 3 - Salada ................... R$ 12,00 |\n"
+                "| 4 - Suco Natural ............. R$ 8,00  |\n"
+                "| 5 - Refrigerante ............. R$ 6,00  |\n"
+                "#=======================================#\n"
+                "Digite o número do seu pedido: ";
+        cin >> opcao;
+
+        switch (opcao) {
+            case 1: cout << "Você pediu: Hamburguer - R$ 15,00\n"; total += 15; break;
+            case 2: cout << "Você pediu: Pizza - R$ 30,00\n"; total += 30; break;
+            case 3: cout << "Você pediu: Salada - R$ 12,00\n"; total += 12; break;
+            case 4: cout << "Você pediu: Suco Natural - R$ 8,00\n"; total += 8; break;
+            case 5: cout << "Você pediu: Refrigerante - R$ 6,00\n"; total += 6; break;
+            default: cout << "Opção inválida. Tente novamente.\n";
+        }
+
+        cout << "\nDeseja fazer outro pedido? (s/n): ";
+        cin >> continuar;
+
+    } while (continuar == 's' || continuar == 'S');
+
+    cout << "\nTotal a pagar: R$ " << total << ",00\n";
+    cout << "Obrigado pela preferência!\n";
 
     return 0;
 }
