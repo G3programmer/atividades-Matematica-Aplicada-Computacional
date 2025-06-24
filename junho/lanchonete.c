c20c03598523a673ab0fd5992e50b1c8027c3d8c
G_03 <130713943+G3programmer@users.noreply.github.com>
2025-06-24 11:39:51 -0300

    Create lanchonete.c

---
+++ junho/lanchonete.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,sub,rep;
+    float total;
+    
+    printf("===== Seja Bem-vindo à Açaiteria do Gabriel Morozini! =====\n");
+    printf("            ===== Menu da lanchonete =====\n");
+    printf("             ---|  Lanches   |--|  Preço  |----|    Bebidas   |--|Preço|---|")
+    printf("             ---| Hambúrguer |--| R$15,00 |----| Refrigerante |--|Preço|---|")
+    printf("             ---|  X-Salada  |--| R$17,00 |----| Suco Natural |--|Preço|---|")
+    printf("             ---|  X-Bacon   |--| R$18,50 |----|~~~~~~~~~~~~~~~~~~~~~~~~~~~|")
+    return 0;
+}
