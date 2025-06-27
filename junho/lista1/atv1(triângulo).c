0a75e436418f342b4abdc282ee60a28b99d2abaa
G_03 <130713943+G3programmer@users.noreply.github.com>
2025-06-27 15:17:27 -0300

    Update atv1(triângulo).c

--- junho/lista1/atv1(triângulo).c
+++ junho/lista1/atv1(triângulo).c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 
 int main() {
-int a,b,c;
-    printf("Digite o valor de 'A': \n");
-    scanf("%d",&a);
-    printf("Digite o valor de 'B': \n");
-    scanf("%d",&b);
-    printf("Digite o valor de 'C': \n");
-    scanf("%d",&c);
+    int a, b, c;
 
-    if(a==b && b==c) {
-    printf("Seu triangulo é um triângulo Equilétero! \n");
-    
+    while (1) {
+        printf("Digite o valor de 'A': \n");
+        scanf("%d", &a);
+        printf("Digite o valor de 'B': \n");
+        scanf("%d", &b);
+        printf("Digite o valor de 'C': \n");
+        scanf("%d", &c);
+
+        if (a > 0 && b > 0 && c > 0) {
+            if (a == b && b == c) {
+                printf("Seu triangulo é um triângulo Equilátero! \n");
+            }
+            else if (a == b || b == c || a == c) {
+                printf("Seu triangulo é um triângulo Isóceles! \n");
+            }
+            else {
+                printf("Seu triangulo é um triângulo Escaleno! \n");
+            }
+            break; // sai do laço se tudo estiver ok
+        } else {
+            printf("Valores inválidos! Tente novamente!\n\n");
+            // o laço continua automaticamente, sem precisar de "continue"
+        }
     }
-    else if(a==b || b==c || a==c){
-        printf("Seu triangulo é um triângulo Isóceles! \n");
-    }
-    
-    else {
-        printf("Seu triangulo é um triângulo Escaleno! \n");
-    }
-    
+
     return 0;
 }
