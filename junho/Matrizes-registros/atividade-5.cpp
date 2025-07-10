b171826e9f3cd417248f497397b1ea08f86b25fe
G_03 <130713943+G3programmer@users.noreply.github.com>
2025-07-10 20:53:57 -0300

    Update atividade-5.cpp

--- junho/Matrizes-registros/atividade-5.cpp
+++ junho/Matrizes-registros/atividade-5.cpp
@@ -1,34 +1,41 @@
 #include <iostream>
 using namespace std;
 
-struct aluno {
+struct Aluno {
     string nome;
-    float nota1,nota2,media;
-    };
+    float nota1, nota2, media;
+};
 
 int main() {
-    const int TAM=3;
-    aluno alunos[TAM];
+    const int TAM = 3;
+    Aluno alunos[TAM];
+    float somaMedias = 0;
     int i;
-    
-    for(i=0;i<TAM;++i) {    
-        cout<<"Digite o nome do aluno: "<<endl;
-        cin>>alunos[i].nome;
-        
-        cout<<"Digite a primeira nota do aluno: "<<endl;
-        cin>>alunos[i].nota1;
-        
-        cout<<"Digite a segunda nota do aluno: "<<endl;
-        cin>>alunos[i].nota2;
-        
-        alunos[i].media=(alunos[i].nota1+alunos[i].nota2)/2;
+    for (i = 0; i < TAM; ++i) {
+        cout << "\nAluno " << i + 1 << endl;
+        cout << "Nome: ";
+        cin >> alunos[i].nome;
+
+        cout << "Nota 1: ";
+        cin >> alunos[i].nota1;
+
+        cout << "Nota 2: ";
+        cin >> alunos[i].nota2;
+
+        alunos[i].media = (alunos[i].nota1 + alunos[i].nota2) / 2;
+        somaMedias += alunos[i].media;
     }
-    
-    for(i=0;i<TAM;++i) {
-        cout<<"Aluno: "<<alunos[i].nome<<"| Média: "<<alunos[i].media<<endl;
+
+    cout << "\n--- Relação Geral ---\n";
+    for (i = 0; i < TAM; ++i) {
+        cout << "Aluno: " << alunos[i].nome
+             << " | Nota 1: " << alunos[i].nota1
+             << " | Nota 2: " << alunos[i].nota2
+             << " | Média: " << alunos[i].media << endl;
     }
-    
-    
-    
+
+    float mediaGeral = somaMedias / TAM;
+    cout << "\nMédia geral da turma: " << mediaGeral << endl;
+
     return 0;
 }
