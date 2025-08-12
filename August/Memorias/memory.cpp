7157c5a81e4298243a002bc0fc90850a57686781
G_03 <130713943+G3programmer@users.noreply.github.com>
2025-08-12 11:21:57 -0300

    Create memory.cpp

---
+++ August/Memorias/memory.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    /* tipo *p=new(tipo)  new é o malloc do c*/
+    // para o free do c, usa delete no c++
+    //new para quando n sabemos o tamanho do vetor
+    
+    int *vet = new int[10];
+}
